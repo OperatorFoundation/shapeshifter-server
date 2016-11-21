1564fc88c1927ef79810dc00e8273ef78b856e92
Brandon Wiley <brandon@blanu.net>
2016-11-21 15:33:31 -0600

    Replaced non-standard rpl commit with standard sed

--- bin/config_client.sh
+++ bin/config_client.sh
@@ -5,6 +5,6 @@ bin/fetch.sh $1 server.crt
 bin/fetch.sh $1 ta.key
 
 cp etc/template.ovpn DO.tblk/DO.ovpn
-rpl my-server $1 DO.tblk/DO.ovpn
+sed -i -e 's/my-server/$1/g' DO.tblk/DO.ovpn
 
 open DO.tblk
