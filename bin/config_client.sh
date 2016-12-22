4ba1da81021b35af56023905b6e4a5b0bea65463
Brandon Wiley <brandon@blanu.net>
2016-12-22 16:54:30 -0600

    Added support for downloading OpenVPN config files into a specified directory

--- bin/config_client.sh
+++ bin/config_client.sh
@@ -1,11 +1,11 @@
-bin/fetch.sh $1 ca.crt
-bin/fetch.sh $1 client1.crt
-bin/fetch.sh $1 client1.key
-bin/fetch.sh $1 server.crt
-bin/fetch.sh $1 ta.key
+bin/fetch.sh $1 ca.crt $2
+bin/fetch.sh $1 client1.crt $2
+bin/fetch.sh $1 client1.key $2
+bin/fetch.sh $1 server.crt $2
+bin/fetch.sh $1 ta.key $2
 
-cp etc/template.ovpn DO.tblk/DO.ovpn
-sed -i -e "s/my-server/$1/g" DO.tblk/DO.ovpn
-rm DO.tblk/DO.ovpn-e
+cp etc/template.ovpn $2/DO.ovpn
+sed -i -e "s/my-server/$1/g" $2/DO.ovpn
+rm $2/DO.ovpn-e
 
-open DO.tblk
+#open $2
