ec809d625bf8458d4a090926ac06664d20ef1ad9
Lita <looklita@gmail.com>
2016-12-23 16:17:19 -0600

    Filepath var fix

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -1,2 +1,2 @@
 echo "scp -i ~/.ssh/id_ecdsa -B -o StrictHostKeyChecking=no root@$1:ca/keys/$2 $3/$2"
-scp -i ~/.ssh/id_ecdsa -B -o StrictHostKeyChecking=no root@$1:ca/keys/$2 $3/$2
+scp -i ~/.ssh/id_ecdsa -B -o StrictHostKeyChecking=no root@$1:ca/keys/$2 "$3/$2"
