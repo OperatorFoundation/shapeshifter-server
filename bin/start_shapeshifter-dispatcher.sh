1a887c259514945c6ba4682b412548ee084e9fa2
Lita <looklita@gmail.com>
2017-01-09 11:29:53 -0600

    Resolved Timing Issue

--- bin/start_shapeshifter-dispatcher.sh
+++ bin/start_shapeshifter-dispatcher.sh
@@ -1,2 +1,10 @@
+echo ">>>>Make file executable"
 chmod +x /etc/init.d/dispatcher
+
+echo ">>>Start the dispatcher"
 /etc/init.d/dispatcher start
+
+echo ">>> Sleeping"
+sleep 5
+
+echo "<<<<End of start_shapeshifter-dispatcher.sh"
