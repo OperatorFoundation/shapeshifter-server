e0a00d804c2b3a1cb95e0c0cdf773b0e5ec98f75
Brandon Wiley <brandon@Adelitas-MacBook-Pro.local>
2017-05-15 16:38:20 -0500

    Switched from init.d scripts to supervisor config files

--- bin/start_shapeshifter-dispatcher.sh
+++ bin/start_shapeshifter-dispatcher.sh
@@ -1,10 +1,5 @@
-echo ">>>>Make file executable"
-chmod +x /etc/init.d/dispatcher
-
-echo ">>>Start the dispatcher"
-/etc/init.d/dispatcher start
-
-echo ">>> Sleeping"
-sleep 30
-
-echo "<<<<End of start_shapeshifter-dispatcher.sh"
+service supervisor restart
+sleep 5
+CERT=`cat /root/go/state/obfs4_bridgeline.txt | grep "^Bridge" | sed "s/=/ /g" | awk '{print $7}'`
+echo $CERT
+echo "{---cert---: ---$CERT---, ---iatMode---: ---0---}" | sed 's/---/"/g' >/root/ca/keys/obfs4.json
