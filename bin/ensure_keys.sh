1b29d933d2b6d6d4b285d538a83a21396bb582ce
Brandon Wiley <brandon@blanu.net>
2017-06-05 13:29:07 -0500

    Deprecated ed25519 SSH keys

--- bin/ensure_keys.sh
+++ bin/ensure_keys.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#if [ ! -f ~/.ssh/id_ed25519 ]; then
-#  ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N '' -q
-#fi
-
 if [ ! -f ~/.ssh/id_ecdsa ]; then
   ssh-keygen -t ecdsa -f ~/.ssh/id_ecdsa -N '' -q
 fi
